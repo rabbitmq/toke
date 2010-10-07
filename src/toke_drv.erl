ea54c597fd31948fd411f133dead9f4fdb345263
Matthew Sackman <matthew@rabbitmq.com>
2010-10-07 14:42:21 +0100

    Make delete in toke idempotent

--- src/toke_drv.erl
+++ src/toke_drv.erl
@@ -30,7 +30,7 @@
          stop/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3,
-	 terminate/2]).
+         terminate/2]).
 
 %% Tokyo Cabinet driver for Erlang
 
