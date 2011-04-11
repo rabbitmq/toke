5d453bc90c010b10d6ecbe3027b5f1360541cb5f
Simon MacMullen <simon@rabbitmq.com>
2011-04-11 16:58:33 +0100

    Check for header files before allowing build.

--- check-deps.sh
+++ check-deps.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #   The contents of this file are subject to the Mozilla Public License
 #   Version 1.1 (the "License"); you may not use this file except in
 #   compliance with the License. You may obtain a copy of the License at
@@ -19,29 +20,21 @@
 #   Contributor(s): ______________________________________.
 #
 
-C_SOURCE_DIR:=$(PACKAGE_DIR)/c_src
-LIBRARY:=$(C_SOURCE_DIR)/libtoke.so
-C_SOURCE:=$(wildcard $(C_SOURCE_DIR)/*.c)
-C_HEADERS:=$(wildcard $(C_SOURCE_DIR)/*.h)
+LOCATIONS='/usr/include /usr/local/include'
+HEADERS='erl_driver.h tcutil.h tchdb.h'
 
-NOT_BUILDABLE:=$(shell toke/check-deps.sh)
+MISSING=
+for HEADER in ${HEADERS} ; do
+    for LOCATION in ${LOCATIONS} ; do
+        if [ -f ${LOCATION}/${HEADER} ] ; then
+            FOUND=true
+        fi
+    done
+    if [ ${FOUND}==false ] ; then
+        MISSING="${MISSING} $HEADER"
+    fi
+done
 
-CC ?= gcc
-CFLAGS ?=
-CC_OPTS:=-Wall -pedantic -std=c99 -O2 -shared -fpic -ltokyocabinet $(CFLAGS)
-
-CONSTRUCT_APP_PREREQS:=$(LIBRARY)
-define construct_app_commands
-	mkdir -p $(APP_DIR)/priv
-	cp $(LIBRARY) $(APP_DIR)/priv
-endef
-
-define package_rules
-
-$(LIBRARY): $(C_SOURCE) $(C_HEADERS)
-	$(CC) $(CC_OPTS) -o $$@ $(C_SOURCE)
-
-$(PACKAGE_DIR)+clean::
-	rm -rf $(LIBRARY)
-
-endef
+if [ "x${MISSING}" != "x" ] ; then
+    echo Headers missing: ${MISSING}
+fi
